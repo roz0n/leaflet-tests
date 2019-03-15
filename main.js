bc77ac6dcf0d17ca220527773d9465700401b321
Arnold Rozon <arnold@rozon.org>
2019-03-15 18:33:58 -0500

    minor syntax changes

--- main.js
+++ main.js
@@ -24,16 +24,13 @@ const onMapClick = e => {
 chiMap.on("click", onMapClick);
 
 const mapboxStyles = {
-  dark: `https://api.mapbox.com/styles/v1/arnoldrozon/cjtap0szn1e5k1fnqqzq6gyp2/tiles/256/{z}/{x}/{y}@2x?access_token=${publicToken}`,
-  basic: `https://api.mapbox.com/styles/v1/arnoldrozon/cjtao21ii1dca1fploetinwop/tiles/256/{z}/{x}/{y}@2x?access_token=${publicToken}`,
+  dark: `https://api.mapbox.com/styles/v1/arnoldrozon/cjtap0szn1e5k1fnqqzq6gyp2/tiles/256/${z}/${x}/${y}@2x?access_token=${publicToken}`,
+  basic: `https://api.mapbox.com/styles/v1/arnoldrozon/cjtao21ii1dca1fploetinwop/tiles/256/${z}/${x}/${y}@2x?access_token=${publicToken}`
 };
 
-L.tileLayer(
-  mapboxStyles.dark,
-  {
-    attribution:
-      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: publicToken
-  }
-).addTo(chiMap);
+L.tileLayer(mapboxStyles.dark, {
+  attribution:
+    'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  accessToken: publicToken
+}).addTo(chiMap);
